3edad2e2204b1d94c4ac25bd77c2e471269ba4a2
Rowan TL <skrublaub@cognitiveweeds.net>
2024-06-12 09:13:04 -0500

    format

--- count.py
+++ count.py
@@ -3,8 +3,9 @@ from typing import Final, Optional
 import re
 import json
 
-INPUT_FILE: Final[Path] = Path("GCA.fna")
+INPUT_FILE: Final[Path] = Path("GCF.fna")
 CHR_NUM_PATTERN: Final[re.Pattern] = re.compile(r"chromosome (\d+|X+|Y+)")
+
 
 def main() -> None:
     # Form of {chromosome label : {k-N # : How many times k-N occures}}
@@ -16,13 +17,13 @@ def main() -> None:
         line: str = rf.readline()
         # line_count: int = 0
         while line:
-            line = line[:-1] # remove the \n at the end of a line
+            line = line[:-1]  # remove the \n at the end of a line
             if line.startswith(">"):
                 res: Optional[re.Match] = CHR_NUM_PATTERN.findall(line)
                 print(res)
                 chromosome = "unplaced" if not res else res[0]
                 if chromosome not in n_count:
-                    n_count[chromosome] = {} # intialize the k-n dict
+                    n_count[chromosome] = {}  # intialize the k-n dict
             else:
                 # This else block will run for a majority of the fasta file
                 for char in line:
@@ -34,12 +35,13 @@ def main() -> None:
                         n_count[chromosome][temp_count] += 1
                         temp_count = 0
 
-            line = rf.readline()            
-            #line_count += 1
-            #print(n_count)                        
+            line = rf.readline()
+            # line_count += 1
+            # print(n_count)
 
     with open("output.json", "w") as wf:
         wf.write(json.dumps(n_count))
-                                    
+
+
 if __name__ == "__main__":
     main()
