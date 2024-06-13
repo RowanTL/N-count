e91f5d47330a8b6af187aa4a297dc99bc4462ede
Rowan TL <skrublaub@cognitiveweeds.net>
2024-06-13 15:11:58 -0500

    fix counting bug

--- count.py
+++ count.py
@@ -7,6 +7,16 @@ INPUT_FILE: Final[Path] = Path("GCF.fna")
 CHR_NUM_PATTERN: Final[re.Pattern] = re.compile(r"chromosome (\d+|X+|Y+)")
 
 
+def add_chromosome(
+    n_count: dict[str, dict[int, int]], chromosome: str, count: int
+) -> dict[str, dict[int, int]]:
+    if count not in n_count[chromosome]:
+        n_count[chromosome][count] = 0
+    n_count[chromosome][count] += 1
+
+    return n_count
+
+
 def main() -> None:
     # Form of {chromosome label : {k-N # : How many times k-N occures}}
     n_count: dict[str, dict[int, int]] = {}
@@ -15,29 +25,37 @@ def main() -> None:
 
     with INPUT_FILE.open("r") as rf:
         line: str = rf.readline()
-        # line_count: int = 0
+        line_count: int = 0
         while line:
             line = line[:-1]  # remove the \n at the end of a line
             if line.startswith(">"):
+                if temp_count != 0: 
+                    n_count = add_chromosome(n_count, chromosome, temp_count)
+                    temp_count = 0            
+
                 res: Optional[re.Match] = CHR_NUM_PATTERN.findall(line)
                 print(res)
+                # old_chromosome: str = chromosome
                 chromosome = "unplaced" if not res else res[0]
                 if chromosome not in n_count:
                     n_count[chromosome] = {}  # intialize the k-n dict
+
+           # This else block will run for a majority of the fasta file
             else:
-                # This else block will run for a majority of the fasta file
                 for char in line:
                     if char == "N":
                         temp_count += 1
                     elif temp_count != 0:
-                        if temp_count not in n_count[chromosome]:
-                            n_count[chromosome][temp_count] = 0
-                        n_count[chromosome][temp_count] += 1
+                        n_count = add_chromosome(n_count, chromosome, temp_count)
                         temp_count = 0
 
             line = rf.readline()
-            # line_count += 1
+            line_count += 1
             # print(n_count)
+
+    # last addition as file could end and Ns could be lost
+    if temp_count != 0:
+        n_count = add_chromosome(n_count, chromosome, temp_count)
 
     with open("output.json", "w") as wf:
         wf.write(json.dumps(n_count))
